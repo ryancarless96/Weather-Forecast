ac05d7dd008384e90f9f93798226a34beac083e6
Ryan Carless <ryancarless96@gmail.com>
2022-11-15 11:00:33 -0500

    commit changes

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+}
