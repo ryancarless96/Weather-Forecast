cb7500e490b613fc04134c252f0a301c26726667
Ryan Carless <ryancarless96@gmail.com>
2022-11-16 20:41:30 -0500

    commit changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,17 +2,15 @@ var resultTextEl = document.querySelector('#result-text');
 var resultAnswersEl = document.querySelector('#result-answer');
 var searchFormEl = document.querySelector('#search-form');
 
-
 function getParams() {
   var searchParamsArr = document.location.search.split('&');
 
   var query = searchParamsArr[0].split('=').pop();
   var format = searchParamsArr[1].split('=').pop();
 
-  searchApi(query,format);
+  searchApi(query, format);
 
 }
-
 function printResults(resultObj) {
   console.log(resultObj);
 
@@ -27,22 +25,60 @@ function printResults(resultObj) {
   titleEl.textContent = resultObj.title;
 
   var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML = 
-  '<strong>Subjects:</strong>' + resultObj.date + '<br/>';
+  bodyContentEl.innerHTML =
+    '<strong>Subjects:</strong>' + resultObj.date + '<br/>';
 
   if (resultObj.subject) {
     bodyContentEl.innerHTML +=
-    '<strong>Subjects:</strong>' + resultObj.subject.join(',') + '<br/>';
+      '<strong>Subjects:</strong>' + resultObj.subject.join(',') + '<br/>';
   } else {
     bodyContentEl.innerHTML +=
-    '<strong> Description: </strong>' + resultObj.description[0];
+      '<strong> Description: </strong>' + resultObj.description[0];
   }
-  if(resultObj.description) {
+  if (resultObj.description) {
     bodyContentEl.innerHTML +=
-    '<strong> Description: </strong>' + resultObj.description[0];
+      '<strong> Description: </strong>' + resultObj.description[0];
   } else {
-    
+    bodyContentEl.innerHTML +=
+      '<strong> Description: </strong> No description for this entry.';
   }
+  var linkButtonEl = document.createElement('a');
+  linkButtonEl.textContent = 'Read More';
+  linkButtonEl.setAttribute('href', resultObj.url)
+  linkButtonEl.classList.add('btn', 'btn-dark');
+
+  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+  resultAnswersEl.append(resultWork);
+}
+function searchApi(query, format) {
+  var locQueryUrl = 'https://openweathermap.org/forecast5';
+  if(format) {
+    locQueryUrl = 'https://www.openweathermap.org/' + format + '/?fo=json';
+  }
+  locQueryUrl = locQueryUrl + '&q=' + query;
+
+  fetch(locQueryUrl)
+  .then(function(response) {
+    if(!response.ok) {
+      throw response.json();
+    }
+    return response.json();
+  })
+  .then(function(locRes){
+    resultTextEl.textContent = locRes.search.query;
+    console.log(locRes);
+
+    if(!locRes.results.length) {
+      console.log('No results found!');
+      resultAnswersEl.textContent = '';
+      for(var i=0; i < locRes.results.length; i++) {
+        printResults(locRes.results[i]);
+      }
+    }
+  })
+  .catch(function(error){
+    console.error(error);
+  });
 }
 function handleSearchFormSubmit(event) {
   event.preventDefault();
@@ -60,4 +96,6 @@ function handleSearchFormSubmit(event) {
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
 
